cff333baac95a33dbafb8553489c8328fe4b2da9
amitkhese <kheseamit@gmail.com>
2015-12-17 18:16:09 +0530

    adeed Generic repository and added menu icons

---
+++ SIMS.Repositories/Interface/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace FC.Repositories
+{
+    /// <summary>
+    /// Defines interface for common data access functionality for entity.
+    /// </summary>
+    /// <typeparam name="TType">Type of entity.</typeparam>
+    public interface IRepository<TType>
+    {
+        TType Add(TType entity);
+
+        bool Update(TType entity);
+
+        bool Delete(TType entity);
+
+        TType Get(Expression<Func<TType, bool>> filter);
+
+        ICollection<TType> GetAll();
+    }
+}
